d98a9fb20fcd52c8f59d3d48427485fbed92e394
Paweenwat srisuk <paweenwat@banjan.ac.th>
2024-03-13 21:41:34 +0700

    add img

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,17 @@
+import Image from "next/image";
+import homeImg from "../../public/home.jpg";
 export default function Home() {
   return (
     <div>
       <h1>Home Page</h1>
+      <div className="absolute -z-10 inset-0">
+        <Image
+          src={homeImg}
+          alt="home page picture"
+          fill
+          style={{ objectFit: "cover" }}
+        />
+      </div>
     </div>
   );
 }
