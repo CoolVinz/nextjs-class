c7ff6c0c82833e90b61c16e0a67c5b384d6fa179
Paweenwat srisuk <paweenwat@banjan.ac.th>
2024-03-14 21:35:59 +0700

    add Hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,11 @@
-import Image from "next/image";
 import homeImg from "../../public/home.jpg";
+import Hero from "@/components/hero";
 export default function Home() {
   return (
-    <div>
-      <h1>Home Page</h1>
-      <div className="absolute -z-10 inset-0">
-        <Image
-          src={homeImg}
-          alt="home page picture"
-          fill
-          style={{ objectFit: "cover" }}
-        />
-      </div>
-    </div>
+    <Hero
+      imgData={homeImg}
+      imgAlt="Car Factory"
+      title="Professional Cloud Hosting"
+    />
   );
 }
