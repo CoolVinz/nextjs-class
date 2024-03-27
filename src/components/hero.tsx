65b5fa1301d91f75eb6d413bb998d57e3a3d636a
Paweenwat srisuk <paweenwat@banjan.ac.th>
2024-03-27 21:10:47 +0700

    fix props

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -6,21 +6,16 @@ interface HeroProps {
   imgAlt: string;
   title: string;
 }
-export default function Hero(props: HeroProps) {
+export default function Hero({ imgData, imgAlt, title }: HeroProps) {
   return (
     <div className="rerative h-screen">
       <div className="absolute -z-10 inset-0">
-        <Image
-          src={props.imgData}
-          alt={props.imgAlt}
-          fill
-          style={{ objectFit: "cover" }}
-        />
+        <Image src={imgData} alt={imgAlt} fill style={{ objectFit: "cover" }} />
         <div className="absolute inset-0 bg-gradient-to-r from-slate-900"></div>
       </div>
 
       <div className="pt-48 flex justify-center items-center">
-        <h1 className="text-white text-6xl">{props.title}</h1>
+        <h1 className="text-white text-6xl">{title}</h1>
       </div>
     </div>
   );
